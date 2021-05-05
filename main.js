855fef94fb28215d3c22bb555e12574a7be36466
Adam Korpen Ström <Korpenxd@gmail.com>
2021-05-06 01:01:06 +0200

    fixed overall alert styling, scaling for mobile. Also tweaked container overflow for better mobile experience in query

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ document.querySelector('.alert').style.display = 'block';
 // Hide alert after 3 seconds
 setTimeout(function(){
   document.querySelector('.alert').style.display = 'none';
-},3000);
+},4000);
 
 // Clear form
 document.getElementById('contactForm').reset();
